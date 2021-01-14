739898429e8f19dc7eb82c97ec26950e8a537c8b
Thinh Nguyen <thinh@vathes.com>
2021-01-14 16:29:07 -0600

    `init.m` to initialize schemas and tables

--- init.m
+++ init.m
@@ -1,9 +1,32 @@
-clear all
+clear all, close all, clc
 
 % Load configuration
 dj.config();
 dj.config.load('dj_local_conf.json')
+cfg = dj.config;
 
-schema_names = {'CF', 'LAB',...
-    'EXP', 'EPHYS',...
-    'MISC', 'ANL'};
+package_names = {'CF', 'LAB','EXP', 'EPHYS', 'MISC', 'ANL'};
+schema_names = {'cf', 'lab','experiment', 'ephys', 'misc', 'analysis'};
+
+% Create schemas 
+for s_idx = 1 : numel(schema_names)
+    dj.createSchema(package_names{s_idx}, './',...
+    [cfg.custom.databasePrefix, schema_names{s_idx}])
+end
+
+% Instantiate tables
+for s_idx = 1 : numel(schema_names)
+   schema_pkg = ['+', package_names{s_idx}];
+   mfiles = dir([schema_pkg, '/*.m']);
+   for f_idx = 1: numel(mfiles)
+       fname = mfiles(f_idx).name(1:end-2);
+       if strcmp(fname, 'getSchema')
+          continue 
+       end
+       disp([package_names{s_idx}, '.', fname])
+       try
+           tmp = eval([package_names{s_idx}, '.', fname, '();']);
+       end
+   end
+end
+
